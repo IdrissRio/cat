3e9792b39b3b2a34bdcf11a13562a4c9c5507814
IdrissRio <riouakidriss@hotmail.it>
2024-12-13 11:48:55 +0100

    Improved README.md

--- run_cat.sh
+++ run_cat.sh
@@ -1,2 +1,3 @@
 echo "Running CAT on CAT's source code. This will take a while... Please wait."
+./gradlew build -x test -PenableOptimization
 java -jar cat.jar build/**/*.java src/**/*.java  -entryPoint org.extendj.Cat main -visualise
